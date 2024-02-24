ae7a8e888f220c9ec39bcf7cdc512e6627915f2e
Cameron Campbell <cameron.campbell325@gmail.com>
2024-02-24 22:19:58 +0000

    Create Settings.lua

---
+++ src/Settings.lua
@@ -0,0 +1,13 @@
+type Settings = {
+	VoxelResolution: number,
+	IsoValue: number,
+	Inverted: boolean
+}
+
+local Settings: Settings = {
+	VoxelResolution = 20,
+	IsoValue = 0,
+	Inverted = false
+}
+
+return Settings
